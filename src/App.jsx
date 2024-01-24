2ccc3c2e0cdee9beb8e326525f6bc695eccc6671
Ayyan <ayyanamir6@gmail.com>
2024-01-24 17:54:37 +0500

    update app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { router } from '@routes';
 import { useSentry, useSetupAxios } from '@services';
 import { AppContext } from '@useContext';
-import { useState } from 'react';
+import { isEmpty } from 'lodash';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
 
   // eslint-disable-next-line no-unused-vars
   const [permissions, setPermissions] = useState('');
+
+  useEffect(() => {
+    console.log(isEmpty({}));
+  }, []);
 
   return (
     <AppContext.Provider value={permissions}>
